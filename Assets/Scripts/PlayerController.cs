924e87ed11e4b7b0a7c7af307b4fd87ba517a85f
IzabranTebras <cristian.fernandezsua@gmail.com>
2016-11-27 17:39:24 +0100

    Added accelereating movement and turning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+enum Movement { Turning = 0, Straight}
+
 public class PlayerController : MonoBehaviour {
-    
+
+    private const float maxSpeed = 10.0f;
+
     public float vSpeed = 0.0f;
-    public float speed = 30.0f;
     public GameObject pointsList;
 
+    private float speed = 1.0f;
+    private Movement movement = Movement.Straight;
+    private float turnY = 0.0f;
     private Transform[] points;
     private int nextPoint = 0;
     private Ray toFollowPoint;
     private bool stop = false;
     private Vector3 targetAngle = Vector3.zero;
 
+    // If player collides with a follow point then assign the new rotation and change to turning movement
     void OnTriggerEnter(Collider coll)
     {
         if (coll.gameObject.CompareTag("NextPoint"))
@@ -21,10 +28,21 @@ public class PlayerController : MonoBehaviour {
             {
                 coll.enabled = false;
                 ++nextPoint;
-               transform.LookAt(points[nextPoint].position);
+                // We check if player must go to left or right
+                if (Vector3.Angle(transform.position, points[nextPoint].position) > 0.0f && Vector3.Angle(transform.position, points[nextPoint].position) <= 90.0f)
+                {
+                    movement = Movement.Turning;
+                    turnY = transform.eulerAngles.y + 90.0f;
+                }
+                else
+                {
+                    movement = Movement.Turning;
+                    turnY = transform.eulerAngles.y - 90.0f;
+                }
             }
             else
             {
+                // If not exists more points then stop the player, is the ending
                 stop = true;
             }
         }
@@ -50,18 +68,35 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    void Update () {
+    void FixedUpdate () {
+
+        switch (movement)
+        {
+            case Movement.Turning:
+                transform.eulerAngles = new Vector3(0.0f, Mathf.LerpAngle(transform.eulerAngles.y, turnY, 0.1f), 0.0f);
+                if(transform.eulerAngles.y == turnY)
+                {
+                    movement = Movement.Straight;
+                }
+                break;
+        }
 
         if (!stop)
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * 10);
+            speed = Mathf.Lerp(speed, maxSpeed, 0.002f);
+            if(speed > maxSpeed)
+            {
+                speed = maxSpeed;
+            }
+
+            transform.Translate(Vector3.forward * speed*0.07f);
             if (Input.GetButtonDown("Jump"))
             {
                 transform.Translate(Vector3.up * 2);
             }
             else if (Input.GetButton("Horizontal"))
             {
-                transform.Translate(Vector3.right * Input.GetAxisRaw("Horizontal")*0.05f);
+                transform.Translate(Vector3.right * Input.GetAxisRaw("Horizontal")*0.07f);
             }
         }
     }
