bda5a38787350ee6b43173defda70940731472f5
IzabranTebras <cristian.fernandezsua@gmail.com>
2016-12-28 01:57:18 +0100

    Added rings, wood behaviour and solved some bugs

---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spring : MonoBehaviour {
+
+    public Rigidbody playerRigid = null;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerRigid.AddForce(Vector3.up*12.0f, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ Assets/Scripts/Wood.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wood : MonoBehaviour {
+
+    public Rigidbody playerRigid = null;
+    public float forceApplied = 12.0f;
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x+ forceApplied*Time.deltaTime, transform.localEulerAngles.y, transform.localEulerAngles.z);
+        transform.Translate(transform.right * Time.deltaTime*speed);
+    }
+
+    void OnColliderEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerRigid.AddForce(Vector3.up * forceApplied, ForceMode.VelocityChange);
+        }
+    }
+}
